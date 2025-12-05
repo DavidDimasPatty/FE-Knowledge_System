05c68f9dc03fe06e2b034d45900b42bece3544fa
DESKTOP-IAV24U9\DAVID <daviddimas80@gmail.com>
2025-12-05 18:54:14 +0700

    Update : Add Sidebar

--- src/components/shared/sideBarRight.js
+++ src/components/shared/sideBarRight.js
@@ -5,7 +5,8 @@ import { FiPlus, FiBook, FiHome, FiSettings, FiStar, FiAlignCenter, FiCode, FiSc
 const SideBarRight = ({ dark }) => {
     const [isSearch, setIsSearch] = useState(false)
 
-    return (<div className={dark ? "w-80 bg-gray-800 p-4" : "w-80 bg-white p-4 border-r"}>
+    return (
+    <div className={dark ? "w-80 bg-gray-800 p-4" : "w-80 bg-white p-4 border-r"}>
 
         <div className="mt-3">
 
@@ -28,7 +29,7 @@ const SideBarRight = ({ dark }) => {
                 </div>
             </div>
 
-            <div className="flex items-center gap-2 mb-2 ml-2  border-b">
+            <div className="flex items-center gap-2 mb-2 ml-2 p-1 border-b">
                 <span className="font-semibold">Saved Topics</span>
             </div>
             <div className="ml-1 space-y-2">
@@ -37,10 +38,10 @@ const SideBarRight = ({ dark }) => {
                 <SidebarItem dark={dark} icon={<BsFillStarFill style={{ color: "yellow", stroke: "black", strokeWidth: "0.6px" }} />} title="Science" desc="Test Description 123" />
             </div>
 
-            <div className="flex items-center gap-2 mb-2  ml-2  border-b ">
+            <div className="flex items-center gap-2 mb-2  ml-2 p-1 border-b ">
                 <span className="font-semibold">Recents</span>
             </div>
-            <div className="ml-1 space-y-2" style={{ maxHeight: "calc(100vh - 400px)", overflowY: "auto" }}>
+            <div className="ml-1 space-y-2" style={{ maxHeight: "calc(100vh - 430px)", overflowY: "auto" }}>
                 <SidebarItem dark={dark} icon={<FiStar />} title="Programming" desc="Test Description 123" />
                 <SidebarItem dark={dark} icon={<FiStar />} title="Education" desc="Test Description 123" />
                 <SidebarItem dark={dark} icon={<FiStar />} title="Science" desc="Test Description 123" />
